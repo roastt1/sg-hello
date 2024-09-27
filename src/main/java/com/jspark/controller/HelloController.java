a0e8a944f15b62e5daac9bb8ed4b526fafaa0238
joonseon <joonseon.p@gmail.com>
2024-09-27 09:47:12 +0900

    commit

--- src/main/java/com/jspark/controller/HelloController.java
+++ src/main/java/com/jspark/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String helloPage(Model model) {
-		model.addAttribute("greeting", "Hello SG!!!");
+		model.addAttribute("greeting", "젠킨스 배포!!!");
 		return "hello";
 	}
 }
